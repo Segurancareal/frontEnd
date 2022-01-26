9b5e66183c81af9d5d66c3bff4b986075051d231
wesley rios <wes.alves.rios@gmail.com>
2022-01-26 11:06:07 -0300

    cadastro

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,10 +11,42 @@ import { Component, OnInit } from '@angular/core';
 export class CadastroComponent implements OnInit {
 
   usuario: Usuario = new Usuario
+  confirmarSenha: string
+  tipoUser: string
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  confirmSenha(event: any) {
+    this.confirmSenha = event.target.value
+  }
+
+  tipoUsuario(event: any) {
+    this.tipoUser = event.target.value
+  }
+
+  cadastrar(){
+    this.usuario.tipo = this.tipoUser
+
+    if(this.usuario.senha != this.confirmarSenha) {
+      alert("As senhas estão incorretas.")
+
+    } else {
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario = resp
+        this.router.navigate(["/login"])
+
+        alert("Usuário Cadastrado com sucesso!")
+      }) 
+
+    }
+
   }
 
 }
