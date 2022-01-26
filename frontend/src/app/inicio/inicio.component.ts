500648e336232e8f6781e2d7baa5650a92c1559e
wesley rios <wes.alves.rios@gmail.com>
2022-01-26 12:10:31 -0300

    Ajustes

--- frontend/src/app/inicio/inicio.component.ts
+++ frontend/src/app/inicio/inicio.component.ts
@@ -15,9 +15,9 @@ export class InicioComponent implements OnInit {
 
   ngOnInit() {
 
-    // if(environment.token == "") {
-    //   this.router.navigate(["/login"])
-    // }
+    if(environment.token == "") {
+      this.router.navigate(["/login"])
+    }
   }
 
 }
