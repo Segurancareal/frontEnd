bd488dcb51d20c1ba0f6b91d01f7f3707a7cb0c5
Joao Victor <luffy2033@outlook.com>
2022-01-26 10:33:56 -0300

    consumo api login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UsuarioLogin = new UsuarioLogin();
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
   }
+
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe((resp: UsuarioLogin)=>{
+      this.userLogin = resp
+
+      environment.token = this.userLogin.token
+      environment.nome = this.userLogin.nome
+      environment.id = this.userLogin.id
+      environment.foto = this.userLogin.foto
+
+      this.router.navigate(["/home"])
+    }, erro =>{
+      if(erro.status == 500){
+        alert("usuario ou senha estao Incorretos!")
+      }
+    })
+  }
+
+
 
 }
