bbf83ebf14970444c70d6e41c421e863576d237e
wesley rios <wes.alves.rios@gmail.com>
2022-01-26 10:29:35 -0300

    service

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
+  usuario: Usuario = new Usuario
+
   constructor() { }
 
   ngOnInit(): void {
