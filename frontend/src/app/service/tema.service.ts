2d2d891f1648388ee919a33d5950de154caf86a3
GuMore <gustavoglm1405@gmail.com>
2022-01-31 09:22:32 -0300

    adicionando o metodo post e get

--- frontend/src/app/service/tema.service.ts
+++ frontend/src/app/service/tema.service.ts
@@ -1,9 +1,28 @@
+import { Tema } from './../model/Tema';
+import { environment } from './../../environments/environment.prod';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TemaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllTema(): Observable<Tema[]>{
+    return this.http.get<Tema[]>('https://segurancareal.herokuapp.com/usuarios/temas', this.token)
+  }
+
+  postTema(tema: Tema): Observable<Tema>{
+    return this.http.post<Tema>('http://localhost:8080/temas', tema, this.token)
+
+  }
+
 }
--- frontend/src/app/tema/tema.component.ts
+++ frontend/src/app/tema/tema.component.ts
@@ -1,4 +1,8 @@
+import { Tema } from './../model/Tema';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemaComponent implements OnInit {
 
-  constructor() { }
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private temaService: TemaService,
+  ) { }
+
+  ngOnInit(){
+
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.tema = new Tema()
+    })
   }
 
 }
