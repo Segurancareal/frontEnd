bf0771a6208c1bac6c5309c3d45f15ed15e519b5
GustavoReis1991 <gustavo.gomes.reis@hotmail.com>
2022-02-01 11:07:55 -0300

    Postagem

--- frontend/src/app/service/auth.service.ts
+++ frontend/src/app/service/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { UsuarioLogin } from '../model/UsuarioLogin';
@@ -13,6 +13,16 @@ export class AuthService {
   constructor(
     private http: HttpClient
   ) { }
+
+  token = {
+    headers: new HttpHeaders().set("Authorization", environment.token)
+  }
+
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+  }
 
   entrar(userLogin: UsuarioLogin): Observable<UsuarioLogin>{
     return this.http.post<UsuarioLogin>('https://segurancareal.herokuapp.com/usuarios/logar', userLogin )
