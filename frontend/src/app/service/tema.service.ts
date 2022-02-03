c28c57bbff5813295f9c2131c7b41eafdec4fe64
Marcelo Augusto <celo.augusto2001@gmail.com>
2022-02-03 17:08:04 -0300

    Ajuste

--- frontend/src/app/service/tema.service.ts
+++ frontend/src/app/service/tema.service.ts
@@ -16,6 +16,12 @@ export class TemaService {
     headers: new HttpHeaders().set('Authorization', environment.token)
   }
 
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+  }
+
   getAllTema(): Observable<Tema[]>{
     return this.http.get<Tema[]>('https://segurancareal.herokuapp.com/temas', this.token)
   }
